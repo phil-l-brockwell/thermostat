b067c1eb720dbe3f84a97b8a138162481427d2a9
Phil Brockwell <nineteen40eight@gmail.com>
2015-03-04 13:55:55 +0000

    up, down, reset buttons working

--- src/thermostatController.js
+++ src/thermostatController.js
@@ -1,5 +1,25 @@
 var thermostat = new Thermostat
 
 var tempDisplay = document.getElementById("temp-display");
-tempDisplay.innerHTML = thermostat.temp + "&deg C" 
+var upButton = document.getElementById('up-button');
+var downButton = document.getElementById('down-button');
+var resetButton = document.getElementById('reset-button');
 
+tempDisplay.innerHTML = thermostat.temp + "&deg C" 
+// var powerSaving = document.querySelector('#power-saving:checked').value;
+
+upButton.addEventListener('click', function() {
+  // var powerSaving = document.querySelector('#power-saving:checked').value;
+  thermostat.increaseTemp();
+  tempDisplay.innerHTML = thermostat.temp + "&deg C" 
+});
+
+downButton.addEventListener('click', function() {
+  thermostat.decreaseTemp();
+  tempDisplay.innerHTML = thermostat.temp + "&deg C" 
+});
+
+resetButton.addEventListener('click', function() {
+  thermostat.resetTemp();
+  tempDisplay.innerHTML = thermostat.temp + "&deg C" 
+});

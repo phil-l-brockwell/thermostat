03e94ae35c01285b510bc94996866ee60718448b
Phil Brockwell <nineteen40eight@gmail.com>
2015-03-04 11:16:34 +0000

    3 tests passing

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -13,8 +13,13 @@ describe('Thermostat', function() {
   });
 
   it('can decrease its temperature', function(){
+    expect(thermostat.temp).toBe(21);
     thermostat.decreaseTemp();
-    expect(thermostat.temp).toBe(19);
+    expect(thermostat.temp).toBe(20);
+  });
+
+  it('has a minimum temperature of 10', function() {
+    expect(thermostat.min).toBe(10);
   });
 
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -5,3 +5,7 @@ Thermostat = function() {
 Thermostat.prototype.increaseTemp = function () {
   this.temp +=1;
 };
+
+Thermostat.prototype.decreaseTemp = function () {
+  this.temp -=1;
+};

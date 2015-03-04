a95cbd76566fb0ef2d20355b6394cb89f879821e
Phil Brockwell <nineteen40eight@gmail.com>
2015-03-04 14:32:02 +0000

    coloured temp

--- src/thermostatController.js
+++ src/thermostatController.js
@@ -1,29 +1,38 @@
 var thermostat = new Thermostat
-
 var tempDisplay = document.getElementById("temp-display");
 var upButton = document.getElementById('up-button');
 var downButton = document.getElementById('down-button');
 var resetButton = document.getElementById('reset-button');
 
-
-tempDisplay.innerHTML = thermostat.temp + "&deg C" 
-
 function checked () {
   return document.getElementById('power-saving').checked;    
 }
 
+function update () {
+  if (thermostat.temp > 25){
+    tempDisplay.style.color = 'red' 
+  } else if (thermostat.temp <= 25 && thermostat.temp > 18) {
+    tempDisplay.style.color = 'yellow'
+  } else if (thermostat.temp <= 18) {
+    tempDisplay.style.color = 'green' 
+  }
+  tempDisplay.innerHTML = thermostat.temp + "&deg C"
+};
+
+update();
+
 upButton.addEventListener('click', function() {
   thermostat.setMode(checked());
   thermostat.increaseTemp();
-  tempDisplay.innerHTML = thermostat.temp + "&deg C" 
+  update();
 });
 
 downButton.addEventListener('click', function() {
   thermostat.decreaseTemp();
-  tempDisplay.innerHTML = thermostat.temp + "&deg C" 
+  update();
 });
 
 resetButton.addEventListener('click', function() {
   thermostat.resetTemp();
-  tempDisplay.innerHTML = thermostat.temp + "&deg C" 
+  update();
 });

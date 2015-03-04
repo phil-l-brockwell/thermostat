ffcb8b6a25fa52f121bfc5c5bbec4f047342164d
Julian Veling <julianveling@Julians-MacBook-Pro.local>
2015-03-04 16:47:33 +0000

    pure js branch

--- src/thermostatController.js
+++ src/thermostatController.js
@@ -1,64 +1,38 @@
-$('document').ready(function() {
-  var thermostat = new Thermostat;
-  $('#temp-display').text(thermostat.temp);
+var thermostat = new Thermostat
+var tempDisplay = document.getElementById("temp-display");
+var upButton = document.getElementById('up-button');
+var downButton = document.getElementById('down-button');
+var resetButton = document.getElementById('reset-button');
 
-  $('#up-button').click(function() {
-    thermostat.setMode(checked());
-    thermostat.increaseTemp();
-    $('#temp-display').text(thermostat.temp);
-  });
+function checked () {
+  return document.getElementById('power-saving').checked;    
+};
 
-  $('#down-button').click(function() {
-    thermostat.decreaseTemp();
-    $('#temp-display').text(thermostat.temp);
-  });
+function update () {
+  if (thermostat.temp > 25){
+    tempDisplay.style.color = 'red' 
+  } else if (thermostat.temp <= 25 && thermostat.temp > 18) {
+    tempDisplay.style.color = 'yellow'
+  } else if (thermostat.temp <= 18) {
+    tempDisplay.style.color = 'green' 
+  }
+  tempDisplay.innerHTML = thermostat.temp + "&deg C"
+};
 
-  $('#reset-button').click(function() {
-    thermostat.resetTemp();
-    $('#temp-display').text(thermostat.temp);
-  });
+update();
 
-  function checked() {
-    return $('#power-saving').is(':checked');
-  };
-
+upButton.addEventListener('click', function() {
+  thermostat.setMode(checked());
+  thermostat.increaseTemp();
+  update();
 });
 
-// var thermostat = new Thermostat
-// var tempDisplay = document.getElementById("temp-display");
-// var upButton = document.getElementById('up-button');
-// var downButton = document.getElementById('down-button');
-// var resetButton = document.getElementById('reset-button');
+downButton.addEventListener('click', function() {
+  thermostat.decreaseTemp();
+  update();
+});
 
-// function checked () {
-//   return document.getElementById('power-saving').checked;    
-// };
-
-// function update () {
-//   if (thermostat.temp > 25){
-//     tempDisplay.style.color = 'red' 
-//   } else if (thermostat.temp <= 25 && thermostat.temp > 18) {
-//     tempDisplay.style.color = 'yellow'
-//   } else if (thermostat.temp <= 18) {
-//     tempDisplay.style.color = 'green' 
-//   }
-//   tempDisplay.innerHTML = thermostat.temp + "&deg C"
-// };
-
-// update();
-
-// upButton.addEventListener('click', function() {
-//   thermostat.setMode(checked());
-//   thermostat.increaseTemp();
-//   update();
-// });
-
-// downButton.addEventListener('click', function() {
-//   thermostat.decreaseTemp();
-//   update();
-// });
-
-// resetButton.addEventListener('click', function() {
-//   thermostat.resetTemp();
-//   update();
-// });
+resetButton.addEventListener('click', function() {
+  thermostat.resetTemp();
+  update();
+});
